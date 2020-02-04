23441f74105f739ebd07d24b576d51fb07344d3e
jeanboy <eesidoro44@gmail.com>
2020-02-04 19:55:32 +0900

    -

---
+++ jgryu/preprocess.py
@@ -0,0 +1,25 @@
+import glob
+import pandas as pd
+
+## parameteres
+data_dir = 'E:/Data/kfood'
+##
+
+def create_metadata(data_dir):
+    file_list = glob.glob(data_dir+'/*/*/*/*')
+    df_meta = pd.DataFrame({'filepath':file_list})
+    le_list = glob.glob(data_dir+'/*/*/*/*')
+    df_meta = pd.DataFrame({'filepath':file_list})
+    df_meta['class1'] = df_meta.filepath.str.extract(r'E:/Data/kfood\\([^\\]+)')
+    df_meta['class2'] = df_meta.filepath.str.extract(r'E:/Data/kfood\\[^\\]+\\([^\\]+)')
+    df_meta['class3'] = df_meta.filepath.str.extract(r'E:/Data/kfood\\[^\\]+\\[^\\]+\\([^\\]+)')
+    df_meta = df_meta.loc[:,['class1','class2','class3','filepath']]
+    df_meta.to_csv('jgryu/data/meta.csv', index=False)
+
+
+def main():
+    create_metadata(data_dir)
+
+
+if __name__ == "__main__":
+    main()
